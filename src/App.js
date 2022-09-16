9706b6fb107a4b4d307d50f02638b11dbfdffb57
martinw0 <68823871+martinw0@users.noreply.github.com>
2022-09-16 17:42:45 +0200

    add react

---
+++ src/App.js
@@ -0,0 +1,75 @@
+import {useState} from 'react';
+import name from './images/name.png';
+import './style.css';
+import AboutMe from './AboutMe';
+import Experiences from './Experiences';
+import Contact from './Contact';
+
+function App() {
+  const [aboutMeSelected, setAboutMeSelected] = useState(true);
+  const handleClickAboutMe = event => {
+    setAboutMeSelected(true);
+    setExperiencesSelected(false);
+    setContactSelected(false);
+  };
+  const [experiencesSelected, setExperiencesSelected] = useState(false);
+  const handleClickExperiences = event => {
+    setExperiencesSelected(true);
+    setAboutMeSelected(false);
+    setContactSelected(false);
+  };
+  const [contactSelected, setContactSelected] = useState(false);
+  const handleClickContact = event => {
+    setContactSelected(true);
+    setAboutMeSelected(false);
+    setExperiencesSelected(false);
+  };
+  return (
+    <div className="App">
+      <header className="header">
+        <nav>
+          <ul>
+            <li onClick={handleClickAboutMe}>About me</li>
+            <li onClick={handleClickExperiences}>Experiences</li>
+            <li onClick={handleClickContact}>Contact</li>
+          </ul>
+        </nav>
+        <img src={name} alt="Martin Wallet" />
+        <h1>Database administrator</h1>
+        <p>But not only</p>
+        <div class="profile">
+          <div class="profile-picture"></div>
+          <ul>
+            <li>
+              <p class="years-number">25</p>
+              <div>
+                <p class="years-category">my age</p>
+                <p class="years-text">years</p>
+              </div>
+            </li>
+            <li>
+              <p class="years-number">5</p>
+              <div>
+                <p class="years-category">studies</p>
+                <p class="years-text">years</p>
+              </div>
+            </li>
+            <li>
+              <p class="years-number">3</p>
+              <div>
+                <p class="years-category">work</p>
+                <p class="years-text">years</p>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </header>
+      {aboutMeSelected && <AboutMe />}
+      {experiencesSelected && <Experiences />}
+      {contactSelected && <Contact />}
+    </div>
+
+  );
+}
+
+export default App;
---
+++ src/Experiences.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class Experiences extends Component {
+    render() {
+        return(
+          <p>Experiences</p>
+        )
+    }
+}
