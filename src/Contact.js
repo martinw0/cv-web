afb84a2d8f0f03371e2db178212ccfa483f1300c
martinw0 <68823871+martinw0@users.noreply.github.com>
2022-09-24 17:59:42 +0200

    build contact

--- src/Contact.js
+++ src/Contact.js
@@ -1,9 +1,34 @@
-import React, { Component } from 'react';
+import React from 'react';
+import './Contact.css';
+import github from './images/github.svg';
+import linkedin from './images/linkedin.svg';
+import twitter from './images/twitter.svg';
+import frontendmentor from './images/frontendmentor.svg';
 
-export default class Contact extends Component {
-    render() {
-        return(
-          <p>Contact</p>
-        )
-    }
+function Contact() {
+  return(
+    <div className='content contact'>
+      <h2>Contact</h2>
+      <div className='social-media'>
+        <div className='link linkedin'>
+          <img src={linkedin} alt='Linked in icon' />
+          <a href=''>Martin Wallet</a>
+        </div>
+        <div className='link github'>
+          <img src={github} alt='GitHub icon' />
+          <a href=''>martinw0</a>
+        </div>
+        <div className='link twitter'>
+          <img src={twitter} alt='Twitter in icon' />
+          <a href=''>@LeWalletM</a>
+        </div>
+        <div className='link frontendmentor'>
+          <img src={frontendmentor} alt='Frontend Mentor icon' />
+          <a href=''>martinw</a>
+        </div>
+      </div>
+    </div>
+  )
 }
+
+export default Contact;
