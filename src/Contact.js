ba13ee138687ce73515d7b8205c26dba12032287
martinw0 <68823871+martinw0@users.noreply.github.com>
2022-10-01 14:35:45 +0200

    transition on path and fixed link in contact

--- src/Contact.js
+++ src/Contact.js
@@ -10,22 +10,30 @@ function Contact() {
     <div className='content contact'>
       <h2>Contact</h2>
       <div className='social-media'>
-        <div className='link linkedin'>
-          <img src={linkedin} alt='Linked in icon' />
-          <a href='https://www.linkedin.com/in/martin-wallet/'>Martin Wallet</a>
-        </div>
-        <div className='link github'>
-          <img src={github} alt='GitHub icon' />
-          <a href='https://github.com/martinw0'>martinw0</a>
-        </div>
-        <div className='link twitter'>
-          <img src={twitter} alt='Twitter in icon' />
-          <a href='https://twitter.com/LeWalletM'>@LeWalletM</a>
-        </div>
-        <div className='link frontendmentor'>
-          <img src={frontendmentor} alt='Frontend Mentor icon' />
-          <a href='https://www.frontendmentor.io/profile/martinw0'>martinw</a>
-        </div>
+        <a href='https://www.linkedin.com/in/martin-wallet/'>
+          <div className='link linkedin'>
+            <img src={linkedin} alt='Linked in icon' />
+            <p>Martin Wallet</p>
+          </div>
+        </a>
+        <a href='https://github.com/martinw0'>
+          <div className='link github'>
+            <img src={github} alt='GitHub icon' />
+            <p>martinw0</p>
+          </div>
+        </a>
+        <a href='https://twitter.com/LeWalletM'>
+          <div className='link twitter'>
+            <img src={twitter} alt='Twitter in icon' />
+            <p>@LeWalletM</p>
+          </div>
+        </a>
+        <a href='https://www.frontendmentor.io/profile/martinw0'>
+          <div className='link frontendmentor'>
+            <img src={frontendmentor} alt='Frontend Mentor icon' />
+            <p>martinw</p>
+          </div>
+        </a>
       </div>
     </div>
   )
