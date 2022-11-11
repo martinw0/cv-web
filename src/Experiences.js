53d32c3f6576f02c91bf7760e89f583613e99e5f
martinw0 <68823871+martinw0@users.noreply.github.com>
2022-11-11 09:04:16 +0100

    finished large desktop

--- src/Experiences.js
+++ src/Experiences.js
@@ -26,155 +26,157 @@ function Experiences() {
         <button onClick={() => stepSelected===3 ? null : setStepSelected(stepSelected+1)}>{t("next")}</button>
       </div>
       <Paragraph step={stepSelected} />
-      <div className='card'>
-        <h3>{t("database_skills")}</h3>
-        <div className='graph'>
-          <div className='skill mysql'>
-            <div className='bar'>
-              <div className='gauge-bg'></div>
-              <div className='gauge'></div>
+      <div className='card-container'>
+        <div className='card'>
+          <h3>{t("database_skills")}</h3>
+          <div className='graph'>
+            <div className='skill mysql'>
+              <div className='bar'>
+                <div className='gauge-bg'></div>
+                <div className='gauge'></div>
+              </div>
+              <p>MySQL</p>
             </div>
-            <p>MySQL</p>
-          </div>
-          <div className='skill postgresql'>
-            <div className='bar'>
-              <div className='gauge-bg'></div>
-              <div className='gauge'></div>
+            <div className='skill postgresql'>
+              <div className='bar'>
+                <div className='gauge-bg'></div>
+                <div className='gauge'></div>
+              </div>
+              <p>PostgreSQL</p>
             </div>
-            <p>PostgreSQL</p>
-          </div>
-          <div className='skill mongodb'>
-            <div className='bar'>
-              <div className='gauge-bg'></div>
-              <div className='gauge'></div>
+            <div className='skill mongodb'>
+              <div className='bar'>
+                <div className='gauge-bg'></div>
+                <div className='gauge'></div>
+              </div>
+              <p>MongoDB</p>
             </div>
-            <p>MongoDB</p>
           </div>
         </div>
-      </div>
-      <div className='card'>
-        <h3>{t("web_skills")}</h3>
+        <div className='card'>
+          <h3>{t("web_skills")}</h3>
 
-        <div className='graph'>
-          <div className='skill html'>
-            <div className='bar'>
-              <div className='gauge-bg'></div>
-              <div className='gauge'></div>
+          <div className='graph'>
+            <div className='skill html'>
+              <div className='bar'>
+                <div className='gauge-bg'></div>
+                <div className='gauge'></div>
+              </div>
+              <p>HTML</p>
             </div>
-            <p>HTML</p>
-          </div>
-          <div className='skill css'>
-            <div className='bar'>
-              <div className='gauge-bg'></div>
-              <div className='gauge'></div>
+            <div className='skill css'>
+              <div className='bar'>
+                <div className='gauge-bg'></div>
+                <div className='gauge'></div>
+              </div>
+              <p>CSS</p>
             </div>
-            <p>CSS</p>
-          </div>
-          <div className='skill js'>
-            <div className='bar'>
-              <div className='gauge-bg'></div>
-              <div className='gauge'></div>
+            <div className='skill js'>
+              <div className='bar'>
+                <div className='gauge-bg'></div>
+                <div className='gauge'></div>
+              </div>
+              <p>JS</p>
             </div>
-            <p>JS</p>
-          </div>
-          <div className='skill sass'>
-            <div className='bar'>
-              <div className='gauge-bg'></div>
-              <div className='gauge'></div>
+            <div className='skill sass'>
+              <div className='bar'>
+                <div className='gauge-bg'></div>
+                <div className='gauge'></div>
+              </div>
+              <p>SASS</p>
             </div>
-            <p>SASS</p>
-          </div>
-          <div className='break'></div>
-          <div className='skill react'>
-            <div className='bar'>
-              <div className='gauge-bg'></div>
-              <div className='gauge'></div>
+            <div className='break'></div>
+            <div className='skill react'>
+              <div className='bar'>
+                <div className='gauge-bg'></div>
+                <div className='gauge'></div>
+              </div>
+              <p>React</p>
             </div>
-            <p>React</p>
-          </div>
-          <div className='skill prestashop'>
-            <div className='bar'>
-              <div className='gauge-bg'></div>
-              <div className='gauge'></div>
+            <div className='skill prestashop'>
+              <div className='bar'>
+                <div className='gauge-bg'></div>
+                <div className='gauge'></div>
+              </div>
+              <p>Prestashop</p>
             </div>
-            <p>Prestashop</p>
-          </div>
-          <div className='skill wordpress'>
-            <div className='bar'>
-              <div className='gauge-bg'></div>
-              <div className='gauge'></div>
+            <div className='skill wordpress'>
+              <div className='bar'>
+                <div className='gauge-bg'></div>
+                <div className='gauge'></div>
+              </div>
+              <p>Wordpress</p>
             </div>
-            <p>Wordpress</p>
           </div>
         </div>
-      </div>
-      <div className='card'>
-        <h3>{t("system_skills")}</h3>
-        <div className='graph'>
-          <div className='skill linux'>
-            <div className='bar'>
-              <div className='gauge-bg'></div>
-              <div className='gauge'></div>
+        <div className='card'>
+          <h3>{t("system_skills")}</h3>
+          <div className='graph'>
+            <div className='skill linux'>
+              <div className='bar'>
+                <div className='gauge-bg'></div>
+                <div className='gauge'></div>
+              </div>
+              <p>Linux</p>
             </div>
-            <p>Linux</p>
-          </div>
-          <div className='skill vmware'>
-            <div className='bar'>
-              <div className='gauge-bg'></div>
-              <div className='gauge'></div>
+            <div className='skill vmware'>
+              <div className='bar'>
+                <div className='gauge-bg'></div>
+                <div className='gauge'></div>
+              </div>
+              <p>VMWare</p>
             </div>
-            <p>VMWare</p>
-          </div>
-          <div className='skill vagrant'>
-            <div className='bar'>
-              <div className='gauge-bg'></div>
-              <div className='gauge'></div>
+            <div className='skill vagrant'>
+              <div className='bar'>
+                <div className='gauge-bg'></div>
+                <div className='gauge'></div>
+              </div>
+              <p>Vagrant</p>
             </div>
-            <p>Vagrant</p>
-          </div>
-          <div className='break'></div>
-          <div className='skill ansible'>
-            <div className='bar'>
-              <div className='gauge-bg'></div>
-              <div className='gauge'></div>
+            <div className='break'></div>
+            <div className='skill ansible'>
+              <div className='bar'>
+                <div className='gauge-bg'></div>
+                <div className='gauge'></div>
+              </div>
+              <p>Ansible</p>
             </div>
-            <p>Ansible</p>
-          </div>
-          <div className='skill molecule'>
-            <div className='bar'>
-              <div className='gauge-bg'></div>
-              <div className='gauge'></div>
+            <div className='skill molecule'>
+              <div className='bar'>
+                <div className='gauge-bg'></div>
+                <div className='gauge'></div>
+              </div>
+              <p>Molecule</p>
             </div>
-            <p>Molecule</p>
-          </div>
-          <div className='skill python'>
-            <div className='bar'>
-              <div className='gauge-bg'></div>
-              <div className='gauge'></div>
+            <div className='skill python'>
+              <div className='bar'>
+                <div className='gauge-bg'></div>
+                <div className='gauge'></div>
+              </div>
+              <p>Python</p>
             </div>
-            <p>Python</p>
-          </div>
-          <div className='break'></div>
-          <div className='skill inspec'>
-            <div className='bar'>
-              <div className='gauge-bg'></div>
-              <div className='gauge'></div>
+            <div className='break'></div>
+            <div className='skill inspec'>
+              <div className='bar'>
+                <div className='gauge-bg'></div>
+                <div className='gauge'></div>
+              </div>
+              <p>Inspec</p>
             </div>
-            <p>Inspec</p>
-          </div>
-          <div className='skill gitlab'>
-            <div className='bar'>
-              <div className='gauge-bg'></div>
-              <div className='gauge'></div>
+            <div className='skill gitlab'>
+              <div className='bar'>
+                <div className='gauge-bg'></div>
+                <div className='gauge'></div>
+              </div>
+              <p>Git CI/CD</p>
             </div>
-            <p>Git CI/CD</p>
-          </div>
-          <div className='skill rpm'>
-            <div className='bar'>
-              <div className='gauge-bg'></div>
-              <div className='gauge'></div>
+            <div className='skill rpm'>
+              <div className='bar'>
+                <div className='gauge-bg'></div>
+                <div className='gauge'></div>
+              </div>
+              <p>RPM</p>
             </div>
-            <p>RPM</p>
           </div>
         </div>
       </div>
